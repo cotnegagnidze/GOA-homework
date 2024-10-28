903642ec2b6db3551f56e8257c0ea4b24aa6a7f4
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2024-10-28 18:05:18 +0400

    second commit

---
+++ lvl 02/homework/homework.py
@@ -0,0 +1,32 @@
+# ცვლადი არის ის რაშიც ჩვენ შეგვაქ მნიშვნელობა
+# ცვლადის სახელი არის ის რასაც ვარქმევთ ცვლადს რომელსაც გააჩნია მნიშვნელობა
+# ცვლადის მნიშვნელობა არის ის რაც ინახება ამ ცვლადში
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+print ("cotnegagnidze")
+
+name = ("cotne")
+surname = ("gagnidze")
+age = 14
+
+print (name)
+print (surname)
+print (age)
+
---
+++ lvl 4/homework/homework.py
@@ -0,0 +1,25 @@
+# python - ში მონაცემები 3 ნაწილად იყოფა string integer float 
+# / ეს არის გაყოფის სიმბოლო % და ეს სიმბოლო არის ნაშთის გამომთვლელი სიმბოლო
+print (10+5)
+print (10-5)
+print (10*5)
+print (10/5)
+print (10**5)
+print (10%5)
+
+Celsius = 20
+print (Celsius*(9/5)+32)
+
+me = 14
+brother =18
+mom = 48
+dad = 51
+
+me1 = (me+20)
+brother1 = (brother+20)
+mom1 = (mom+20)
+dad1 = (dad+20)
+
+
+
+

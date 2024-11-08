7106ba0855d341fb798d601353eafa0066c0405b
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2024-11-08 19:48:26 +0400

    second commit

---
+++ lvl 10/classwork/classwork.py
@@ -0,0 +1,12 @@
+
+
+
+
+height = float(input("Please enter your height in centimeters: "))
+print(height) 
+
+weight = float(input("Please enter your weight in kilograms: "))
+print(weight)
+
+bmi = (weight/height**2)
+print (bmi)
---
+++ lvl 10/homework/homework.py
@@ -0,0 +1,40 @@
+uhm = float(input("hdrhrdga"))
+print (uhm)
+
+hum = int(input("fahkfwahgfgakw"))
+print (hum)
+
+muh = str(input("fwafawgwgawgda"))
+print (muh)
+
+# მონაცემთა ტიპის კონვერტაცია მაშინ გვჭირდება როდესაც გვინდა რომ მონაცეს ტიპი შევუცვალოთ მაგალითად float ჩავწეროთ როგორც integer
+
+a = 10 > 5
+print (a)
+
+b = 10 < 5
+print (b)
+
+c = 5 >= 7
+print (c)
+
+d = 20 <= 5
+print (d)
+
+e = 15 == 15
+print (e)
+
+f = 12 != 17
+print (f)
+
+
+age = int(input("please enter your age"))
+print (age+10)
+
+age1 = int(input("please enter your age"))
+
+age2= int(input("please enter your age"))
+
+age3 = int(input("please enter your age"))
+
+print ((age1+age2+age3)/3)

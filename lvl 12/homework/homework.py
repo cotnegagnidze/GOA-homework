f2da029c773d149719affd38d12cfaef65bbbe6c
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2024-11-22 20:11:17 +0400

    second commit

--- lvl 12/homework/homework.py
+++ lvl 12/homework/homework.py
@@ -5,3 +5,7 @@ print (15 > 27 and 170 >= 207)
 print (12 > 8 or 30 == 27)
 print (19 > 10 and 180 <= 17)
 
+# control flow არის კონკრეტული მიმვდერობა რომლის მიხედვითაც იწერება კოდი და მას გააჩნია 3 ტექნიკა: sequences iterations and selections
+
+
+# N7 ver gavige :(
---
+++ lvl 14/homework/homework.py
@@ -0,0 +1,21 @@
+# ციკლი არის რაღაც დანიშნული ბრძანების განმეორებით გაკეთება
+# იტერაცია არის განცხადებების სიმრავლის განმეორებით შესრულებას 
+
+for range in (20,50):
+    print (range)
+
+
+for range1 in (20,50,2):
+    print (range1)
+
+for range2 in (100,150):
+    print (range)
+
+for range1 in (11,31,2):
+    print (range1)
+
+for i in range (10):
+    print("cotne gagnidze")
+
+for range3 in (10,30):
+    print (i/2)

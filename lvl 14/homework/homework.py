090bdd4512db7311260486699be7857cbc25069e
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2024-11-26 16:52:43 +0400

    second commit

--- lvl 14/homework/homework.py
+++ lvl 14/homework/homework.py
@@ -1,21 +1,34 @@
 # ციკლი არის რაღაც დანიშნული ბრძანების განმეორებით გაკეთება
 # იტერაცია არის განცხადებების სიმრავლის განმეორებით შესრულებას 
 
-for range in (20,50):
-    print (range)
+# for range in (20,50):
+#     print (range)
 
 
-for range1 in (20,50,2):
-    print (range1)
+# for range1 in (20,50,2):
+#     print (range1)
 
-for range2 in (100,150):
-    print (range)
+# for range2 in (100,150):
+#     print (range)
 
-for range1 in (11,31,2):
-    print (range1)
+# for range5 in (11,31,2):
+#     print (range1)
 
-for i in range (10):
-    print("cotne gagnidze")
+# for i in range (10):
+#     print("cotne gagnidze")
 
-for range3 in (10,30):
-    print (i/2)
+# for range3 in (10,30):
+#     print (i/2)
+
+# for i in range (40,60):
+    # print (i**3)
+
+# name = input("enter your name")
+
+# for i in range(20):
+    # print(name)
+    # name = "alex"
+
+# for i in name:
+    # print(i)
+

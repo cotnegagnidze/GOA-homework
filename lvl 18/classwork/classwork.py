b0bee4f56bfc421f418a1b8d551f10293eafc38b
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2024-12-06 18:16:46 +0400

    second commit

---
+++ lvl 18/classwork/classwork.py
@@ -0,0 +1,23 @@
+# for i in range (0, 20 ,2):
+#     print (i)
+
+
+# for i in range (0, 20 ,1):
+#     print (i)
+
+
+# for i in range (10, 30):
+#     if range (10, 30, 2):
+#         print ("even")
+#         if range (11, 30, 1):
+#             print ("odd")
+
+
+num1= input("enter a number")
+num2= input("enter a number")
+factorial, starting_number=1,1
+if num1 > num2:
+    print (range(int(num2, num1)))
+
+
+
---
+++ lvl 18/homework/homework.py
@@ -0,0 +1,40 @@
+# num1 = int(input("enter a number"))
+# num2 = int(input("enter a number"))
+
+# if num1 >= num2:
+#     print (num1)
+# else:
+#     print (num2)
+
+
+
+# num1 = int(input("Enter number: "))
+# if num1 %2 == 0:
+#     print(str(num1), "is even")
+# else:
+#     print(str(num1), "is odd")
+
+
+# num1 = int(input("Enter number: "))
+# if num1  >= 0:
+#     print(str(num1), "is a positive number")
+# else:
+#     print(str(num1), "is a negative number")
+
+
+# num1 = int(input("Enter number: "))
+# if num1 / 5 == 0:
+#     print ("this number is not divisable by 5")
+# else:
+#     print ("this number is divisable by 5")
+
+
+
+correct_password ="GOA BEST"
+while True: 
+    password = input ("password")
+
+if password == correct_password:
+    print ("correct password")
+else:
+    print ("incorrect password. try again")

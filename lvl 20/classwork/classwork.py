9837bf9f17272b334a9b00080f4acd5eebeb339b
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2024-12-13 16:22:23 +0400

    second commit

---
+++ lvl 20/classwork/classwork.py
@@ -0,0 +1,5 @@
+input3 = int(input("enter a number"))
+input1 = int(input("enter a number"))
+input2 = int(input("enter a number"))
+list[input3, input1, input2]
+print (list)
---
+++ lvl 20/homework/homework.py
@@ -0,0 +1,50 @@
+# ჩვენ შეგვიძლია if-ის შემდეგ დავწეროთ elif else-ის მაგივრად და elif-ით შეგვიძლია 1-ზე მეტი დავალება მივცეთ if თეგს
+
+
+# num1 = float(input("enter a number"))
+# num2 = float(input("enter a number"))
+# num3 = float(input("enter a number"))
+# if num1 > num2 > num3:
+#     print (num1)
+# elif num2 > num1 > num3:
+#     print (num2)
+# else:
+#     print (num3)
+
+
+
+# num1 = float(input("enter a number"))
+# if num1 >= 90:
+#     print ("A")
+# elif num1 > 80 and num1 < 89:
+#     print ("B")
+# elif num1 > 70 and num1 < 79:
+#     print ("C")
+# elif num1 > 60 and num1 < 69:
+#     print ("D")
+# else:
+#     print ("F")
+
+
+# num1 = float(input("enter a number"))
+# if num1 > 0:
+#     print (num1, "is a positive number")
+# elif num1 < 0:
+#     print (num1, "is a negative number")
+# else:
+#     print ("zero")
+
+# num1 = int(input("enter a number"))
+# num2 = int(input("enter a number"))
+# if num1 > num2:
+#     print (num1, ">", num2)
+# else:
+#     print (num1, "<", num2)
+
+list3 = [1, 2, 3, 4, 5]
+num1 = list3[0]
+print (num1)
+num2 = list3[2]
+print (num2)
+num3 = list3[4]
+print (num3)

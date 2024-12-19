47f6de5f84acd4b060ab10a0729dd3c7922a94a5
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2024-12-19 21:57:00 +0400

    second commit

---
+++ lvl 22/classwork/classwork.py
@@ -0,0 +1,4 @@
+list1 = [ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+for i in list1:
+    print (i * 2)
+    print (i / 2)
---
+++ lvl 22/homework/homework.py
@@ -0,0 +1,18 @@
+# num1 = list[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in num1:
+#     print (i)
+
+
+# num1 = [10, 20, 30, 40, 50]
+
+# print("First element:", numbers[0])   
+# print("Third element:", numbers[2])  
+# print("Last element:", numbers[-1])
+
+str = ["apple", "banana", "cherry", "date"]
+
+
+strings = str[::-1]
+
+
+print(strings)

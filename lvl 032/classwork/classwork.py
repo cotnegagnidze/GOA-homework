b02abd4571f5afc40c80f964347e6dacab06e09e
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2025-01-28 16:35:06 +0400

    second commit

---
+++ lvl 032/classwork/classwork.py
@@ -0,0 +1,46 @@
+# def generate_big_sentence(name, surname, role):
+#     return "My name is {} my surname is {} my role in academy is {}.".format(name, surname, role)
+
+
+# name = "cotne"
+# surname = "gagnidze"
+# role = "student"
+
+# sentence = generate_big_sentence(name, surname, role)
+# print(sentence)
+
+
+# def generate_big_sentence(name, surname, age):
+#     return (f"My name is {name} {surname}, and I am {age} years old.")
+
+
+# name = "cotne"
+# surname = "gagnidze"
+# age = 14
+
+
+# sentence = generate_big_sentence(name, surname, age)
+# print(sentence)
+
+
+
+# def my_split(main_string, string_to_split):
+#     split_result = main_string.split(string_to_split)
+#     print("დაყოფილი სია:", split_result)
+
+# main_string = input("მიუთითეთ მთავარი სტრინგი: ")
+# string_to_split = input("მიუთითეთ დასაყოფი სტრინგი: ")
+
+# my_split(main_string, string_to_split)
+
+
+
+# def manual_append(main_list, item_to_insert):
+#     main_list.insert(len(main_list), item_to_insert)
+
+
+# example_list = [1, 2, 3]
+# manual_append(example_list, 4)
+# print(example_list)
+
+
---
+++ lvl 032/homework/homework.py
@@ -0,0 +1,69 @@
+# def welcome (name, age):
+#     return (f"welcome {name}. happy birthday you are are already {age} years old!")
+
+
+
+# greet = welcome ("cotne", 14)
+# print (greet)
+
+
+
+# def format_name(first_name, last_name):
+#     return f"{first_name.capitalize()} {last_name.capitalize()}"
+
+# print(format_name("cotne", "gagnidze"))
+
+
+# def reverse_and_format(text):
+#     return f"The reversed string is: {text[::-1]}"
+
+# print(reverse_and_format("hello"))
+
+
+# def insert_word(sentence, word, index):
+#     return sentence[:index] + word + sentence[index:]
+
+# print(insert_word("Hello world!", " amazing", 5))
+
+# def get_words(sentence):
+#     return sentence.split()
+
+# print(get_words("idk what to say"))
+
+# def csv_to_list(csv):
+#     return csv.split(',')
+
+# print(csv_to_list("apple,banana,orange"))
+
+# def split_into_sentences(paragraph):
+#     return paragraph.split('.')
+
+# print(split_into_sentences("This is the first sentence. This is the second sentence."))  
+
+# def append_item(lst, item):
+#     lst.append(item)
+#     return lst
+
+
+# print(append_item([1, 2, 3], 4))
+
+# def append_items(lst, items):
+#     lst.extend(items)
+#     return lst
+
+# print(append_items([1, 2, 3], [4, 5]))
+
+# def insert_at_start(lst, item):
+#     lst.insert(0, item)
+#     return lst
+
+# print(insert_at_start([2, 3, 4], 1))
+
+
+# def insert_at_end(lst, item):
+#     lst.insert(len(lst), item)
+#     return lst
+
+# print(insert_at_end([1, 2, 3], 4))
+
+

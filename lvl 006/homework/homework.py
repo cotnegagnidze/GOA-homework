8988a8e9d7bf9d74ee8e12e05fe67016214c033d
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2025-04-22 14:17:46 +0400

    second commit

--- lvl 006/homework/homework.py
+++ lvl 006/homework/homework.py
@@ -12,3 +12,14 @@ c = 20
 (c + 4)
 (c + 89)
 print (c)
+
+# camel case- python
+# snake case- python
+# kebab case- css
+# pascal case- ???
+# macro case- ???
+# train case- python
+# debuging - არის შეცდომების გასწორება
+# არსებობს case sensitive და case insensitive პროგრამირების ენები და case sensitive ენებში კოდში მნიშვნელობა აქვს ჩვენ დიდ თუ პატარა ასოს გამოვიყენებთ და case insensitive ენაში ამას მნიშვნელობა არ აქვს
+# case sensitive = C C++ JAVA
+# case insensitive = ABAP ADA BASICs

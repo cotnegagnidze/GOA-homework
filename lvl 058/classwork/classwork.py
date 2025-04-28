cb18ccaab0c4770a7f066778a8f4f26b56ae1fd2
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2025-04-28 19:02:03 +0400

    second commit

--- lvl 058/classwork/classwork.py
+++ lvl 058/classwork/classwork.py
@@ -1,3 +1,5 @@
+import string
+
 
 reverse_and_capitalize = lambda s: s[::-1].capitalize()
 
@@ -10,3 +12,4 @@ print((lambda x: x * 2)(10))
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(list(map(lambda x: x ** 2, numbers)))
+

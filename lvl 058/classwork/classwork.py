644b76f8d6bea820a23fad12e48e4af540a12700
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2025-05-06 20:52:34 +0400

    second commit:

--- lvl 058/classwork/classwork.py
+++ lvl 058/classwork/classwork.py
@@ -13,3 +13,11 @@ print((lambda x: x * 2)(10))
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(list(map(lambda x: x ** 2, numbers)))
 
+def weather_info (temp):
+    c = (temp)
+    if (c < 0):
+        return (c," is freezing temperature")
+    else:
+        return (c," is above freezing temperature")
+
+print(weather_info(-10))
---
+++ lvl 060/homework/homework.py
@@ -0,0 +1,16 @@
+list1 = [1, "ghseg", 4, 1, 1]
+# list1.append(True)
+# print (list1)
+
+# list1.remove(4)
+# print (list1)
+
+# list1.pop(1)
+# print (list1)
+
+# list1.reverse()
+# print (list1)
+
+# count = list1.count(1)
+# print (count)
+

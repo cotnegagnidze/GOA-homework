0eabedac5e5b3f37dced7535c7f51a66dc6c3a66
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2025-05-12 15:39:35 +0400

    second commit

---
+++ lvl 063/homework/homework.js
@@ -0,0 +1,32 @@
+// function aler () {
+//     alert("hello world")
+// }
+
+// aler()
+
+// function greet (name) {
+//     alert("hello " + name)
+// }
+
+// greet ("cotne")
+
+// function count (num1, num2) {
+//     console.log (num1 + num2)
+// }
+
+// count (5, 11)
+
+// function x10 (num1) {
+//     console.log (num1  * 10)
+// }
+
+// x10 (5, 11)
+
+// function divide(num1, num2) {
+//     console.log(num1 / num2)
+// }
+
+// function change () {
+//     let a = document.getElementById("p")
+//     a.textContent = "salami"
+// }

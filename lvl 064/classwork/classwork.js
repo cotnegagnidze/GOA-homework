afca724221bf218dbf880340a5be8148ee08759a
cotnegagnidze <cotnecotnegagnidzegagnidze@gmail.com>
2025-05-13 23:10:10 +0400

    second commit

---
+++ lvl 064/classwork/classwork.js
@@ -0,0 +1,14 @@
+// function handleSubmit(event) {
+//   event.preventDefault();
+
+//   let input = document.getElementById('username');
+//   console.log("input", input.value);
+// }
+
+function concStrings () {
+    let a = prompt("enter smth")
+    let x = prompt("enter smth 2")
+    let z = alert(a + x)
+}
+
+concStrings()
---
+++ lvl 064/homework/homework.js
@@ -0,0 +1,39 @@
+// let a = prompt("what is ur hobby")
+// alert(a)
+
+// let a = prompt("enter ur name")
+// let x = prompt("enter ur surname")
+
+// alert(a + x)
+
+// let a = prompt("Enter your message:");
+// document.querySelector("p").textContent = a;
+
+// let x = prompt("enter ur fav emoji")
+// alert (x + "thank u")
+
+
+// let a = prompt("enter website title")
+// document.title = a
+
+// document.getElementById('btn6').addEventListener('click', () => {
+//   const txt = document.getElementById('textInput').value;
+//   alert(txt);
+// });
+
+// document.getElementById('form7').addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const color = document.getElementById('bgColor').value;
+//   document.body.style.backgroundColor = color;
+// });
+
+
+// document.getElementById('btn8').addEventListener('click', () => {
+//   const first = document.getElementById('firstName').value.trim();
+//   const last  = document.getElementById('lastName').value.trim();
+//   if (!first || !last) {
+//     alert('Please fill out both names!');
+//     return;
+//   }
+//   document.getElementById('fullName').textContent = `${first} ${last}`;
+// });
